4a23f81544dbe20eabf3f92d2d68c64038057195
youneccon <53691447+youneccon@users.noreply.github.com>
2019-08-16 17:42:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 $(function(){
+  console.log('header top_____'+$('header').offset().bottom);
+  console.log('nav_top_____'+$('nav').offset().top);
+  console.log('nav_bottom____'+$('nav').offset().bottom);
+  console.log('main__top___'+$('main').offset().top);
 });
