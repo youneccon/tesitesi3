627edb4209aa7430fb82e2cd39ee5b21d533a39c
youneccon <53691447+youneccon@users.noreply.github.com>
2019-08-16 18:18:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,65 @@ $(function(){
   console.log('nav_top_____'+$('nav').offset().top);
   console.log('nav_bottom____'+$('nav').offset().bottom);
   console.log('main__top___'+$('main').offset().top);
+    function getRandom(min, max) {
+    var random = Math.floor(Math.random() * (max + 1 - min)) + min;
+
+    return random;
+  }
+
+  function patrol(brothers, targetClass) {
+    let nb = 1;
+    let totalNumber = $(brothers).length;
+    $(brothers).each(function(index) {
+
+      if ($(this).hasClass(targetClass)) {
+        nb = (index + 1) % totalNumber;
+        $(this).removeClass(targetClass);
+      }
+    });
+
+    $(brothers).each(function(index) {
+      if (nb === index) {
+        $(this).addClass(targetClass);
+      }
+    });
+  }
+
+  function slideShow(brothers, show) {
+    let zIndex = [];
+    $(brothers).each(function() {
+      zIndex.push(parseInt($(this).css('z-index')));
+    });
+    let length = zIndex.length;
+    let maxIndex = zIndex.indexOf(Math.max.apply(null, zIndex));
+    let randomArray = [];
+    for (i = 0; i < length; i++) {
+      if (i != maxIndex) {
+        randomArray.push(i);
+      }
+    }
+
+    let nb = randomArray[getRandom(0, randomArray.length - 1)];
+
+    $(brothers).each(function(index) {
+      $(this).removeClass(show);
+      if (index === nb) {
+        $(this).css('z-index', zIndex.length - 1)
+        $(this).addClass(show);
+      } else if (zIndex[index] > zIndex[nb]) {
+        $(this).css('z-index', zIndex[index] - 1)
+      }
+    });
+
+  };
+
+  setInterval(function() {
+    patrol('.catchcopyContainer p', 'sharp');
+  }, 4000);
+
+  setTimeout(function() {
+    setInterval(function() {
+      slideShow(".slide", 'playingSlide');
+    }, 10000);
+  }, 5000);
 });
